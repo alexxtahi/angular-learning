49b7bb7ba1e4e55f0c3915887019ad3df9f19271
Alexandre TAHI <62557152+alexxtahi@users.noreply.github.com>
2023-09-10 11:17:50 +0000

    Patch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: `<h1>Welcome to {{ pokemons[1] }}!</h1>`,
+  templateUrl: "./app.component.html",
 })
 export class AppComponent implements OnInit {
   // Properties
@@ -14,8 +14,13 @@ export class AppComponent implements OnInit {
     "Charmander",
     "Charmeleon",
   ];
+
   // Methods
   ngOnInit(): void {
     console.table(this.pokemons);
+  }
+
+  selectPokemon(name: string): void {
+    console.log(`You selected ${name}`);
   }
 }
